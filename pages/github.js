9402b7d604b4289f7a8b1406473747bd3c4fbcb0
Agustin Paolantonio <agustinpaolantonio@gmail.com>
2022-08-03 18:15:25 -0300

    first commit

---
+++ pages/github.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+const Github = () => (
+  <Layout>
+    <h1>Hello Github!!</h1>
+  </Layout>
+);
+
+export default Github;
---
+++ pages/hireme.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+const Hireme = () => (
+  <Layout>
+    <h1>HireMe!!</h1>
+  </Layout>
+);
+
+export default Hireme;
